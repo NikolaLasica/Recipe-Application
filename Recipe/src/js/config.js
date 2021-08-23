3c37299220f16feac629632522ce281a5182cd1e
NikolaLasica <88530769+NikolaLasica@users.noreply.github.com>
2021-08-24 01:45:43 +0200

    Add files via upload

---
+++ Recipe/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7ea3f3c9-7567-43f3-9659-a19dbc3af743';
+export const MODAL_CLOSE_SEC = 2.5;
